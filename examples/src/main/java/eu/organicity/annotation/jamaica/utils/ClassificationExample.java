506ba65a29a24aac221e02fb0f0e62cba5d9ce29
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2016-05-19 10:33:06 +0300

    jubatus training and testing examples initial commit

---
+++ examples/src/main/java/eu/organicity/annotation/jamaica/utils/ClassificationExample.java
@@ -0,0 +1,101 @@
+package eu.organicity.annotation.jamaica.utils;
+
+import us.jubat.classifier.ClassifierClient;
+import us.jubat.classifier.EstimateResult;
+import us.jubat.classifier.LabeledDatum;
+import us.jubat.common.Datum;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassificationExample {
+
+
+    private static Datum makeDatum(double height) {
+        return new Datum().addNumber("value", height);
+    }
+
+    private static LabeledDatum makeTrainDatum(String label, double height) {
+        return new LabeledDatum(label, makeDatum(height));
+    }
+
+    public static void main(String[] args) throws Exception {
+        //Connect to the server
+        String host = "150.140.5.98";
+        int port = 9199;
+        String name = "test";
+
+        ClassifierClient client = new ClassifierClient(host, port, name, 1);
+
+        //Build the train dataset from the input file
+        List<LabeledDatum> trainData = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("humidity.classified"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String label = line.split(",")[0];
+                Double value = Double.valueOf(line.split(",")[1]);
+                trainData.add(makeTrainDatum(label, value));
+                br.readLine();
+                br.readLine();
+                br.readLine();
+                br.readLine();
+                br.readLine();
+                br.readLine();
+                br.readLine();
+                br.readLine();
+                br.readLine();
+                br.readLine();
+                br.readLine();
+                br.readLine();
+                br.readLine();
+                br.readLine();
+                //train in batches to avoid timeouts
+                if (trainData.size() == 100) {
+                    client.train(trainData);
+                    trainData.clear();
+                }
+            }
+
+        } catch (Exception e) {
+        }
+
+
+        double correct = 0;
+        double total = 0;
+        //test data
+        try (BufferedReader br = new BufferedReader(new FileReader("humidity.classified"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                final String label = line.split(",")[0];
+                final Double value = Double.valueOf(line.split(",")[1]);
+
+                final List<Datum> testData = new ArrayList<>();
+                testData.add(makeDatum(value));
+                List<List<EstimateResult>> results = client.classify(testData);
+                double maxScore = Double.MIN_NORMAL;
+                String maxLabel = "";
+                for (List<EstimateResult> result : results) {
+                    for (EstimateResult r : result) {
+                        if (r.score > maxScore) {
+                            maxScore = r.score;
+                            maxLabel = r.label;
+                        }
+                    }
+                    if (maxLabel.equals(label)) {
+                        correct++;
+                    }
+                    total++;
+                    System.out.println("correct : " + correct);
+                    System.out.println("total : " + total);
+                    System.out.println("score : " + correct / total);
+                }
+            }
+        } catch (Exception e) {
+        }
+
+
+        System.exit(0);
+    }
+}
