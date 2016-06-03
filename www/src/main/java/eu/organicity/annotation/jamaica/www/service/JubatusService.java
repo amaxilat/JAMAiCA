ecb2b64a31c1fdb420f85eab36f36ffc8ca0a6a7
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2016-06-03 11:46:17 +0300

    add placeholder PostConstruct method

--- www/src/main/java/eu/organicity/annotation/jamaica/www/service/JubatusService.java
+++ www/src/main/java/eu/organicity/annotation/jamaica/www/service/JubatusService.java
@@ -1,9 +1,21 @@
 package eu.organicity.annotation.jamaica.www.service;
 
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 @Service
 public class JubatusService {
+    /**
+     * a log4j logger to print messages.
+     */
+    protected static final Logger LOGGER = Logger.getLogger(JubatusService.class);
+
+    @PostConstruct
+    public void init() {
+        LOGGER.debug("init");
+    }
 
 }
