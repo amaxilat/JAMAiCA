51bf6a627e69a124e2ff3604631ff3a39f3986be
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2016-06-07 14:37:22 +0300

    add jubatus launcher calls in the JubatusService

--- www/src/main/java/eu/organicity/annotation/jamaica/www/service/JubatusService.java
+++ www/src/main/java/eu/organicity/annotation/jamaica/www/service/JubatusService.java
@@ -18,4 +18,34 @@ public class JubatusService {
         LOGGER.debug("init");
     }
 
+    /**
+     * Launches a jubaclassifier instance in the given port.
+     *
+     * @param port the port for the jubaclassified RPC interface.
+     * @return the {@see Process} object of the launched jubaclassifier instance.
+     */
+    public Process launchJubaclassifier(int port) {
+        try {
+            return Runtime.getRuntime().exec("jubaclassifier -p " + port + " -f classifier.json");
+        } catch (Exception e) {
+            LOGGER.error(e, e);
+            return null;
+        }
+    }
+
+    /**
+     * Launches a jubaanomaly instance in the given port.
+     *
+     * @param port the port for the jubaanomaly RPC interface.
+     * @return the {@see Process} object of the launched jubaanomaly instance.
+     */
+    public Process launchJubaanomaly(int port) {
+        try {
+            return Runtime.getRuntime().exec("jubaanomaly -p " + port + " -f anomaly.json");
+        } catch (Exception e) {
+            LOGGER.error(e, e);
+            return null;
+        }
+    }
+
 }
