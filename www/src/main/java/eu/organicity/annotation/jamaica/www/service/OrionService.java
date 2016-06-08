1a6cac1053fe0bea0a03d7393e76c9aeecd18791
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2016-06-08 12:28:13 +0300

    move orion configuration to the application.yml

--- www/src/main/java/eu/organicity/annotation/jamaica/www/service/OrionService.java
+++ www/src/main/java/eu/organicity/annotation/jamaica/www/service/OrionService.java
@@ -7,34 +7,37 @@ package eu.organicity.annotation.jamaica.www.service;
 import com.amaxilatis.orion.OrionClient;
 import com.amaxilatis.orion.model.subscribe.OrionEntity;
 import com.amaxilatis.orion.model.subscribe.SubscriptionResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 
 @Service
 public class OrionService {
 
+    @Value("${orion.serverUrl}")
     private String serverUrl;
-    private String token ;
-    private String service ;
+
+    @Value("${orion.token}")
+    private String token;
+
+    @Value("${orion.service}")
+    private String service;
+
+    @Value("${orion.servicePath}")
     private String servicePath;
+
     private OrionClient client;
 
 
     @PostConstruct
     public void init() {
-
-        this.serverUrl = "http://195.220.224.30:1026/";
-        this.token = "";
-        this.service = "organicity";
-        this.servicePath = "/";
         this.client = new OrionClient(this.serverUrl, this.token, this.service, this.servicePath);
-
     }
 
-    public SubscriptionResponse subscribeToOrion(OrionEntity entity, String[] attributes, String reference, String[] conditions, String duration) throws IOException
-    {
-        return client.subscribeChange(entity,attributes, reference, conditions, duration);
+    public SubscriptionResponse subscribeToOrion(OrionEntity entity, String[] attributes, String reference, String[] conditions, String duration) throws IOException {
+        return client.subscribeChange(entity, attributes, reference, conditions, duration);
     }
 
 }
