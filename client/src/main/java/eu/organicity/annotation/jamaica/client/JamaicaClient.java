989c834e6b61cdfc4ce3ce17ba98ec1b2179f69f
Deligiannidou Katerina <deligian.kat@gmail.com>
2016-07-07 13:03:44 +0300

    Jamaica client put and delete configs

--- client/src/main/java/eu/organicity/annotation/jamaica/client/JamaicaClient.java
+++ client/src/main/java/eu/organicity/annotation/jamaica/client/JamaicaClient.java
@@ -1,6 +1,7 @@
 package eu.organicity.annotation.jamaica.client;
 
 import eu.organicity.annotation.jamaica.dto.AnomalyConfigDTO;
+import eu.organicity.annotation.jamaica.dto.ClassifConfigDTO;
 import org.springframework.web.client.RestTemplate;
 
 public class JamaicaClient {
@@ -16,4 +17,29 @@ public class JamaicaClient {
         return restTemplate.getForObject(BASE_URL + "config/anomaly/" + id, AnomalyConfigDTO.class);
     }
 
+    public void deleteAnomalyConfig(final long id){
+        restTemplate.delete(BASE_URL + "config/anomaly/" + id, AnomalyConfigDTO.class);
+    }
+
+    public AnomalyConfigDTO putAnomalyConfig(AnomalyConfigDTO anomalyConfig){
+
+        restTemplate.put(BASE_URL + "config/anomaly/", anomalyConfig, AnomalyConfigDTO.class);
+        return anomalyConfig;
+    }
+
+    public ClassifConfigDTO getClassificationConfig(final long id){
+        return restTemplate.getForObject(BASE_URL + "config/classification/" + id, ClassifConfigDTO.class);
+
+    }
+
+    public void deleteClassificationConfig(final long id){
+        restTemplate.delete(BASE_URL + "config/classification/" + id, ClassifConfigDTO.class);
+
+    }
+
+    public ClassifConfigDTO putClassificationConfig(ClassifConfigDTO classificationConfig){
+
+        restTemplate.put(BASE_URL + "config/classification/", classificationConfig, ClassifConfigDTO.class);
+        return classificationConfig;
+    }
 }
