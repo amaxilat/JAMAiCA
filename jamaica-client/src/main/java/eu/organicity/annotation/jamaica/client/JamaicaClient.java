93dd47658842f70dc4ccfedb5d00f123ec1b252d
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2016-12-19 12:54:12 +0200

    cleanup pages

--- jamaica-client/src/main/java/eu/organicity/annotation/jamaica/client/JamaicaClient.java
+++
@@ -1,7 +0,0 @@
-package eu.organicity.annotation.jamaica.client;
-
-public class JamaicaClient extends JamaicaAndroidClient {
-    public JamaicaClient(String token) {
-        super(token);
-    }
-}
